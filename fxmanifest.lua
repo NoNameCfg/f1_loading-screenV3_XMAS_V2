ce08c9534f805ef7851b56b7d2b48ee436f3588c
R3site <61720260+R3site@users.noreply.github.com>
2023-01-05 20:15:37 -0500

    Fix loading screen staying on screen
    Server does not know to run main.lua if not declared as client script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,8 @@ author "F1"
 description "discord.gg/Nb23tC3wmA - XMAS EDITION V2"
 version "3.0.2"
 
+client_script 'main.lua'
+
 files {
     'index.html',
     'audio/*.mp3',
@@ -14,7 +16,6 @@ files {
     'js/*.js',
     'js/snowStorm.js',
     'js/snowStorm-min.js',
-    'main.lua',
 }
 loadscreen_manual_shutdown 'yes'
 loadscreen 'index.html'
